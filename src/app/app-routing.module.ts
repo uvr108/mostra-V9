f8df0f051a2db8c4f2661b77e2782d08dcb26a07
uvergara <uvergara@csn.uchile.cl>
2020-03-06 09:30:19 -0500

    Actualizaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnalisisComponent } from './analisis/analisis.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+ { path:'analisis', component: AnalisisComponent },
+ { path: '', redirectTo: '/analisis',  pathMatch: 'full' },
+ { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 export class AppRoutingModule { }
