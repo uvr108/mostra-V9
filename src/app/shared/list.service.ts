4cc0b10d31767e3f533755dd3e3997fe6b519bc3
uvergara <uvergara@csn.uchile.cl>
2020-03-10 08:56:48 -0400

    Nested solution

--- src/app/shared/list.service.ts
+++ src/app/shared/list.service.ts
@@ -8,20 +8,39 @@ import { retry, catchError } from 'rxjs/operators';
 })
 export class ListService {
 
+  baseurl = 'http://10.54.223.18/api2db/?has-preferred=true&include-descriptions=true';
+
   // baseurl = 'http://localhost/api2db/?order-by=time-desc';
 
-  baseurl = 'http://localhost/api2db/5321/email_info';
-
+  // baseurl = 'http://10.54.223.18/api2db/';
   
+  // baseurl = 'http://10.54.223.18/api2db/?has-preferred=true&include-descriptions=true'
+  // + '&start-time=2020-02-01T00:00:00Z&end-time=2020-04-20T15:00:00Z&order-by=time-desc';
+  
+  // baseurl = 'http://127.0.0.1/api2db/5321/email_info';
 
-  getList(): Observable<Array<{}>> {
-      console.log(this.baseurl);
-      return this.http.get<any>(this.baseurl)
+  // 2020-02-01T00:00:00Z
+  // 2020-04-20T15:00:00Z
+ 
+  getList(ini: string, fin: string): Observable<Array<{}>> {
+      
+      return this.http.get<any>(this.baseurl + '&start-time=' + ini + '&end-time=' + fin + '&order-by=time-desc')
       .pipe(
         retry(1),
         catchError(this.errorHandl)
       );
   }
+
+  
+  GetIssue(value): Observable<[{}]> {
+    console.log('http://10.54.223.18/api2db/' + value + '/email_info');
+    return this.http.get<any>('http://10.54.223.18/api2db/' + value + '/email_info')
+    .pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    );
+  }
+  
 
   constructor(private http: HttpClient) { }
 
